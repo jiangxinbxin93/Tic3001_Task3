146d5c16bf07969c6b04a4d1b00f554bf80af75c
Junhui Tang <tang_junhui_93@hotmail.com>
2023-03-30 16:24:56 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,141 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
+
+const users = [
+  {
+    id: "1",
+    username: "Kyle",
+    password: "321",
+    isAdmin: true,
+  },
+  {
+    id: "2",
+    username: "Jim",
+    password: "789",
+    isAdmin: false,
+  },
+];
+
+let refreshTokens = [];
+
+const verify = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err) {
+        return res.status(403).json("Token is not valid");
+      }
+
+      req.user = user;
+      next();
+    });
+  } else {
+    res.status(401).json("You are not authenticated");
+  }
+};
+
+app.post("/api/refresh", (req, res) => {
+  const refreshToken = req.body.token;
+
+  if (!refreshToken) return res.status(401).json("You are not authenticated!");
+  if(!refreshTokens.includes(refreshToken)){
+    return res.status(403).json("Refresh token is not valid");
+
+  }
+  jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET,(err,user)=>{
+    err && console.log(err);
+    refreshTokens= refreshTokens.filter((token)=>token!==refreshToken);
+
+    const newAccessToken = generateAccessToken(user);
+    const newRefreshToken= generateRefreshToken(user);
+
+    refreshTokens.push(newRefreshToken);
+
+    res.status(200).json({
+      accessToken:newAccessToken,
+      refreshToken:newRefreshToken
+    })
+  })
+
+});
+
+const generateAccessToken = (user) => {
+  return jwt.sign(
+    { id: user.id, isAdmin: user.isAdmin },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "15m" }
+  );
+};
+
+const generateRefreshToken = (user) => {
+  return jwt.sign(
+    { id: user.id, isAdmin: user.isAdmin },
+    process.env.REFRESH_TOKEN_SECRET
+  );
+};
+
+app.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find((u) => {
+    return u.username === username && u.password === password;
+  });
+  if (user) {
+    const accessToken=generateAccessToken(user);
+    const refreshToken=generateRefreshToken(user);
+    refreshTokens.push(refreshToken);
+    res.json({
+      username: user.username,
+      isAdmin: user.isAdmin,
+      accessToken,
+      refreshToken
+    });
+  } else {
+    res.status(400).json("Username or password incorrect");
+  }
+});
+
+
+app.post("/api/logout",verify,(req,res)=>{
+  const refreshToken=req.body.token;
+  refreshTokens=refreshTokens.filter((token)=>token!==refreshToken);
+  res.status(200).json("You logged out successfully");
+
+})
+
+app.get("/api/edit/:userId", verify, (req, res) => {
+  if (req.user.id === req.params.userId || req.user.isAdmin) {
+    res.status(200).json("Welcome to the edit page");
+  } else {
+    res.status(403).json("Only admin have the right to access edit page!");
+  }
+
+});
+
+
+
+
+
+
+
+
+
+
+app.delete("/api/users/:userId", verify, (req, res) => {
+  if (req.user.id === req.params.userId || req.user.isAdmin) {
+    res.status(200).json("User has been deleted");
+  } else {
+    res.status(403).json("You are not allowed to delete this user!");
+  }
+});
+
+
+
+app.listen(3000);
